a9b2c5b2668dfc12df9971d0c41151e735ebd274
Yudhistira Haryatmaka Santoso <84625580+disyudhis@users.noreply.github.com>
2023-06-13 00:45:10 +0700

    making game's ui

--- Assets/Script/EnemyHit.cs
+++ Assets/Script/EnemyHit.cs
@@ -1,14 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EnemyHit : MonoBehaviour
 {
     [SerializeField] private GameObject enemyDied;
+    [SerializeField] private GameController gameController;
+    [SerializeField] private GameObject popupCanvas;
+
+    private void Awake()
+    {
+        gameController = FindObjectOfType<GameController>();
+    }
 
     public void EnemyKilled()
     {
         Instantiate(enemyDied, transform.position, transform.rotation);
+        //calculate the score for hitting this enemy
+        float distanceFromPlayer = Vector3.Distance(transform.position, Vector3.zero);
+        int bonusPoints = (int)distanceFromPlayer;
+
+        int enemyScore = 10 * bonusPoints;
+
+        //set our text for the popup - instantiate popup canvas
+        popupCanvas.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = enemyScore.ToString();
+        GameObject enemyPopup = Instantiate(popupCanvas, transform.position, Quaternion.identity);
+
+        //adjust the scale of the popup
+        enemyPopup.transform.localScale = new Vector3(transform.localScale.x * (distanceFromPlayer / 2),
+            transform.localScale.y * (distanceFromPlayer / 2),
+            transform.localScale.z * (distanceFromPlayer / 2));
+
+        //pass score to GameController
+        gameController.UpdatePlayerScore(enemyScore);
+
         Destroy(this.gameObject);
     }
 }
--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] private Image timerImage;
     [SerializeField] private float gameTime;
     private float sliderCurrentFillAmount = 1f;
+
+    [Header("Score Components")]
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    private int playerScore;
 
     private void Update()
     {
@@ -17,5 +23,11 @@ public class GameController : MonoBehaviour
         timerImage.fillAmount = sliderCurrentFillAmount - (Time.deltaTime / gameTime);
 
         sliderCurrentFillAmount = timerImage.fillAmount;
+    }
+
+    public void UpdatePlayerScore(int enemyHitPoints)
+    {
+        playerScore += enemyHitPoints;
+        scoreText.text = playerScore.ToString();
     }
 }
